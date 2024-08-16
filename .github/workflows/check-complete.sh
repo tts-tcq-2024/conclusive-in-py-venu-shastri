84a4a45737188c3e949ba3c266f53b996d21f4b0
Venugopal K <venu.shastri@gmail.com>
2024-08-16 12:37:06 +0530

    Update check-complete.sh

--- .github/workflows/check-complete.sh
+++ .github/workflows/check-complete.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
+set -e
 
-# Check if any .md file contains the string "_enter"
 if grep -q _enter *.md; then
-  # Inform the user to replace the text
-  echo "Replace all text containing '_enter' with your input"
-  # Exit with a status code of 1 (indicating the need for user action)
+  echo "Replace all text having _enter with your input"
   exit 1
 fi
-
-# If no action is required, the script will continue here
